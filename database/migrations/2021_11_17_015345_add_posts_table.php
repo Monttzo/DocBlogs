0176719c3cab978d1734c0f1902da576ec8be2f1
Monttzo <dpulido59@gmail.com>
2021-11-18 22:03:26 -0500

    Migracion Post Cambio
    Se aumenta el tamaño de la descripción a 400 caracteres

--- database/migrations/2021_11_17_015345_add_posts_table.php
+++ database/migrations/2021_11_17_015345_add_posts_table.php
@@ -16,7 +16,7 @@ class AddPostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->string('description', 400);
 
             $table->unsignedBigInteger('user_id');
 
